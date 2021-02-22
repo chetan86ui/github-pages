121c5954068dacd6f4a1eebe0b4a2ada81bfe940
chetan <chetan86ui@gmail.com>
2021-02-21 18:35:20 -0800

    sort logic apply

--- src/components/ProblemListing.js
+++ src/components/ProblemListing.js
@@ -1,21 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function ProblemListing(props) {
-    const problemsprop = props.problemprops;
-    console.log("hello",problemsprop);
+const ProblemListing = (props) => {
+
+    
+    const [problemsprop, setproblemsprop] = useState(props.problemprops);
+    const compare = (a, b) => {
+        const bandA = a.level.toUpperCase();
+        const bandB = b.level.toUpperCase();
+      
+        let comparison = 0;
+        if (bandA > bandB) {
+          comparison = 1;
+        } else if (bandA < bandB) {
+          comparison = -1;
+        }
+        return comparison;
+      };
+      //problemsprop.sort(compare);
+      const sortArray = () => {
+        const newArr = problemsprop.sort(compare);
+        console.log("newArr",newArr);
+        setproblemsprop(newArr);
+        //problemsprop.sort(compare);
+        
+      };
+    //   const sorted = [...problemsprop].sort((a, b) => {
+    //     return b.level - a.level;
+    //   });
+    //   setproblemsprop(sorted);
+    // <div>
+    //         Sort : <button onClick={ sortArray }>Sort</button>
+    //     </div>
     return (
-        <div className="grid">
+        <>
+        <div>
+            Sort : <button onClick={ sortArray }>Sort</button>
+        </div>
+        <div className="grid1">
             {
-                problemsprop.map((item) => {
+                problemsprop.map((item,index) => {
                     return (
-                        <div className="item" key={item.id}>
-                            <div className="ptitle">{ item.title } <span data-level={ item.level } className="pbedge">{ item.level }</span></div>
-                            
+        
+                        <div className="item1" key={item.id}>
+                            <div className="item-id">{index+1}</div>
+                            <div className="item-title">{ item.title }</div>
+                            <div className="item-bedge"><span className="pbedge" data-level={ item.level }>{ item.level }</span></div>
                         </div>
+                        
                     )
                 })
             }
         </div>
+        </>
     )
 }
 
